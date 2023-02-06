559d7b7f5653db7732c301fe6a24d65c779f8e5a
jjjjiho <wlgh2381@gmail.com>
2023-02-06 17:59:06 +0900

    clothes

---
+++ np1.js
@@ -0,0 +1,11 @@
+document.querySelector('.bt2').addEventListener('click', function(){
+    document.querySelector('.container1').style.transform='translate(-100vw)';
+});
+document.querySelector('.bt3').addEventListener('click', function(){
+    document.querySelector('.container1').style.transform='translate(-200vw)';
+});
+document.querySelector('.bt1').addEventListener('click', function(){
+    document.querySelector('.container1').style.transform='translate(0)';
+});
+
+
---
+++ np2.js
@@ -0,0 +1,9 @@
+document.querySelector('.bt5').addEventListener('click', function () {
+  document.querySelector('.container2').style.transform = 'translate(-100vw)';
+});
+document.querySelector('.bt6').addEventListener('click', function () {
+  document.querySelector('.container2').style.transform = 'translate(-200vw)';
+});
+document.querySelector('.bt4').addEventListener('click', function () {
+  document.querySelector('.container2').style.transform = 'translate(0)';
+});
